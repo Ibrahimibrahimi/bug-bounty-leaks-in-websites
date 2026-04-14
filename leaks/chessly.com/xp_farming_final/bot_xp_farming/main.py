afa2f76ba197813b326d23c838636372bd74df45
Ibrahimibrahimi <ibrahim.id-wahman.06@edu.uiz.ac.ma>
2026-04-14 17:23:14 +0100

    some changes

--- leaks/chessly.com/xp_farming_final/bot_xp_farming/main.py
+++ leaks/chessly.com/xp_farming_final/bot_xp_farming/main.py
@@ -6,7 +6,8 @@ cookies = getCookies()
 
 
 # exrtact all gotham bot uuids
-bot_uuids = []
+bot_uuids = [str(uuid.uuid4()) for i in range(50)]
 
 for bot_game in bot_uuids :
     win_bot_game(bot_game,cookies)
+    time.sleep(2)
